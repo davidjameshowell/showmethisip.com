0e0817a250a3909c91caaf48dd0dc9d2d12fb698
David Howell <david@davidjameshowell.com>
2021-09-18 07:46:29 +0000

    Update poetry lock and packages, update script with CI flag

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,11 +4,14 @@ STAGE="${STAGE-development}"
 TAG="${TAG-v0.0.1}"
 REGION="${AWS_REGION:-us-east-1}"
 REPOSITORY_NAME="showmethisip"
+CI_JOB="${CI_JOB:-true}"
+
+if [[ $CI_JOB == *"true"* ]]; then
+    POETRY_VIRTUALENVS_CREATE=false && poetry install --no-root
+fi
 
 FIND_ECR_REPOSITORY=$(aws --region=us-east-1 ecr describe-repositories --repository-name ${REPOSITORY_NAME})
 REPOSITORY_ID=$(echo ${FIND_ECR_REPOSITORY} | jq --raw-output '.repositories[].registryId')
-
-#ZAPPA_STATUS=$(zappa status ${STAGE})
 
 if ZAPPA_STATUS=$(zappa status ${STAGE} 2>&1); then
     zappa update "${STAGE}" -d "${REPOSITORY_ID}".dkr.ecr."${REGION}".amazonaws.com/"${REPOSITORY_NAME}":"${STAGE}"-${TAG}
